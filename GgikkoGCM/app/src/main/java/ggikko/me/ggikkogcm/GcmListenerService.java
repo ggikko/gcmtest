3383f9abfa934bfd86603cc3eb93bc503cdf4153
ggikko <ggikko@hotmail.com>
2015-12-02 13:23:30 +0900

    gcm test second

--- GgikkoGCM/app/src/main/java/ggikko/me/ggikkogcm/GcmListenerService.java
+++ GgikkoGCM/app/src/main/java/ggikko/me/ggikkogcm/GcmListenerService.java
@@ -1,7 +1,52 @@
 package ggikko.me.ggikkogcm;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
 /**
  * Created by ggikko on 2015. 12. 2..
  */
-public class GcmListenerService {
+
+public class GcmListenerService extends com.google.android.gms.gcm.GcmListenerService {
+
+    //디바이스에 받은 메세지를 내용을 정의하는 클래슨
+    private static final String TAG = "GcmListenerService";
+    private static final int REQUEST_CODE = 0;
+    private static final int NOTIFICATION_ID = 0;
+
+    @Override
+    public void onMessageReceived(String from, Bundle data) {
+
+        String title = data.getString("title");
+        String message = data.getString("message");
+
+        sendNotification(title, message);
+    }
+
+    private void sendNotification(String title, String message) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, REQUEST_CODE, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.opened4)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(NOTIFICATION_ID, notificationBuilder.build());
+    }
 }
--- GgikkoGCM/app/src/main/java/ggikko/me/ggikkogcm/InstanceIDListenerService.java
+++ GgikkoGCM/app/src/main/java/ggikko/me/ggikkogcm/InstanceIDListenerService.java
@@ -1,7 +1,19 @@
 package ggikko.me.ggikkogcm;
 
+import android.content.Intent;
+
 /**
  * Created by ggikko on 2015. 12. 2..
  */
-public class InstanceIDListenerService {
+public class InstanceIDListenerService extends com.google.android.gms.iid.InstanceIDListenerService {
+
+    //Instance ID를 얻을 리스너를 상속받고 토큰 갱신함
+
+    private static final String TAG = "InstanceIDListenerService";
+
+    @Override
+    public void onTokenRefresh() {
+        Intent intent = new Intent(this, RegisterationIntentService.class);
+        startService(intent);
+    }
 }
